eef996c753596a5567881155583687be98f286a7
Robby Tarigan <robbytarigan@hotmail.com>
2016-06-19 23:08:55 +0100

    Calculate perimeter and area of a rectangle.

---
+++ 03OperatorsAndExpressions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Side: ");
+            var side = float.Parse(Console.ReadLine());
+        
+            Console.WriteLine("Enter Height");
+            var height = float.Parse(Console.ReadLine());
+        
+            Console.WriteLine("Perimeter\t: {0}", 2 * (side + height));
+            Console.WriteLine("Area\t\t: {0}", side * height);
+        }
+    }
+}
